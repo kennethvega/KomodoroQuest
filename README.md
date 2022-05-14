d622a09c94105075f0d8db8df6f34129b6f6d6f0
kenneth vega <kenneth.trinidad.vega@gmail.com>
2022-05-14 19:49:30 +0800

    just added the react router and making a navbar

---
+++ README.md
@@ -0,0 +1,1 @@
+use git to track changes modified
--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Signup from "./pages/signup/Signup";
+import Login from "./pages/login/Login";
+
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
